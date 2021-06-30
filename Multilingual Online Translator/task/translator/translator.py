3dc7f4985e2d73fbd6439acab39d14f394dce53e
jairo8925 <csarmiento2333@gmail.com>
2021-06-29 23:09:42 -0700

    Initial commit

---
+++ Multilingual Online Translator/task/translator/translator.py
@@ -0,0 +1,6 @@
+print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
+language = input()
+print("Type the word you want to translate:")
+word = input()
+
+print('You chose "' + language + '" as the language to translate "' + word + '" to.')
