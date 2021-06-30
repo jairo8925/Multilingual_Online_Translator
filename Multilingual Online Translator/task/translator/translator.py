5c92de6e417bf777b10985112f30fb87440e2dc2
jairo8925 <csarmiento2333@gmail.com>
2021-06-30 04:48:47 -0700

    made results output more readable by separating sentences and translations by newlines. also only outputs 5 translations and 5 sentence pairs

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -9,28 +9,35 @@ word = input()
 
 print('You chose "' + language + '" as the language to translate "' + word + '" to.')
 
+
 if language == "en":
     url = "https://context.reverso.net/translation/french-english/" + word
+    language = "English"
 else:
     url = "https://context.reverso.net/translation/english-french/" + word
+    language = "French"
 
 r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
 if r.status_code == 200:
     print("200 OK")
-    print("Translations")
+    print()
+    print(language, "Translations:")
+
     soup = BeautifulSoup(r.content, "html.parser")
     translations = []
     div = soup.find("div", {"id": "translations-content"})
     for a in div.find_all("a"):
         translations.append(a.text.strip())
-    print(translations)
+    print("\n".join(translations[:5]))
+    print()
 
     examples = []
     section = soup.find("section", {"id": "examples-content"})
     for span in section.find_all("span", {"class": "text"}):
         examples.append(span.text.strip())
-    print(examples)
+    print(language, "Examples:")
+    print("\n\n".join(("\n".join(j for j in examples[i:i+2]) for i in range(0, 10, 2))))
 
 else:
     print("Try again?")
