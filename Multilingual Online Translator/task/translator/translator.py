6d6e7d3fc55b28f96159653c6423b7ce2e438be4
jairo8925 <csarmiento2333@gmail.com>
2021-06-30 13:30:48 -0700

    added language support for 13 languages

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,28 +1,41 @@
 from bs4 import BeautifulSoup
 import requests
 
+languages = {
+    '1': 'arabic',
+    '2': 'german',
+    '3': 'english',
+    '4': 'spanish',
+    '5': 'french',
+    '6': 'hebrew',
+    '7': 'japanese',
+    '8': 'dutch',
+    '9': 'polish',
+    '10': 'portuguese',
+    '11': 'romanian',
+    '12': 'russian',
+    '13': 'turkish'
+}
 
-print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
-language = input()
+print("Hello, you're welcome to the translator. Translator supports:")
+for k, v in languages.items():
+    print(k + ". " + v.capitalize())
+
+print("Type the number of your language:")
+language_from = input()
+print("Type the number of language you want to translate to:")
+language_to = input()
 print("Type the word you want to translate:")
 word = input()
 
-print('You chose "' + language + '" as the language to translate "' + word + '" to.')
-
-
-if language == "en":
-    url = "https://context.reverso.net/translation/french-english/" + word
-    language = "English"
-else:
-    url = "https://context.reverso.net/translation/english-french/" + word
-    language = "French"
+url = "https://context.reverso.net/translation/" + languages.get(language_from) + "-" + languages.get(language_to) + "/" + word
+language_to = languages.get(language_to).capitalize()
 
 r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
 if r.status_code == 200:
-    print("200 OK")
     print()
-    print(language, "Translations:")
+    print(language_to, "Translations:")
 
     soup = BeautifulSoup(r.content, "html.parser")
     translations = []
@@ -36,7 +49,7 @@ if r.status_code == 200:
     section = soup.find("section", {"id": "examples-content"})
     for span in section.find_all("span", {"class": "text"}):
         examples.append(span.text.strip())
-    print(language, "Examples:")
+    print(language_to, "Examples:")
     print("\n\n".join(("\n".join(j for j in examples[i:i+2]) for i in range(0, 10, 2))))
 
 else:
